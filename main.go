2ae3950f17cd1120525d1b74c816ef3bc4d76508
Александр Аксютин <162790771+Ky6una@users.noreply.github.com>
2025-02-16 19:12:14 +0300

    Первая реализация post и get запросов

--- main.go
+++ main.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+var task string
+
+type requestBody struct {
+	Message string `json:"message"`
+}
+
+func GetHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello %s !!!", task)
+}
+
+func PostHandler(w http.ResponseWriter, r *http.Request) {
+	var req requestBody                          //переменная для хранекия данных из тела запроса
+	decoder := json.NewDecoder(r.Body)           //создаём декодер JSON, привязанный к телу запроса r.Body
+	if err := decoder.Decode(&req); err != nil { //декодируем JSON из тела запроса в переменную req, при возникновении ошибки возвращаем код 400
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+	task = req.Message
+	w.WriteHeader(http.StatusOK) //устанавливаем статус ответа 200
 }
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/api/hello", HelloHandler).Methods("GET")
+	router.HandleFunc("/api/task", GetHandler).Methods("GET")
+	router.HandleFunc("/api/task", PostHandler).Methods("POST")
+
 	http.ListenAndServe(":8080", router)
 
 }
