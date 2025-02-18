45dd4b6c90e9c50b80226059782b1e236361fd93
Александр Аксютин <162790771+Ky6una@users.noreply.github.com>
2025-02-18 16:18:36 +0300

    Добавление БД

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
 )
@@ -14,25 +13,49 @@ type requestBody struct {
 }
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello %s !!!", task)
+	var tasks []Task
+	if err := DB.Find(&tasks).Error; err != nil {
+		http.Error(w, "Failed to get tasks list", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tasks)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	var req requestBody                          //переменная для хранекия данных из тела запроса
-	decoder := json.NewDecoder(r.Body)           //создаём декодер JSON, привязанный к телу запроса r.Body
-	if err := decoder.Decode(&req); err != nil { //декодируем JSON из тела запроса в переменную req, при возникновении ошибки возвращаем код 400
-		http.Error(w, "Invalid request", http.StatusBadRequest)
+	var t Task
+	if arr := json.NewDecoder(r.Body).Decode(&t); arr != nil {
+		http.Error(w, "Invalid recording format", http.StatusBadRequest)
 		return
 	}
-	task = req.Message
-	w.WriteHeader(http.StatusOK) //устанавливаем статус ответа 200
+	if err := DB.Create(&t).Error; err != nil {
+		http.Error(w, "Failed to save task", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(t); err != nil {
+		http.Error(w, "Failed to encode task", http.StatusInternalServerError)
+	}
+	//var req requestBody                          //переменная для хранекия данных из тела запроса
+	//decoder := json.NewDecoder(r.Body)           //создаём декодер JSON, привязанный к телу запроса r.Body
+	//if err := decoder.Decode(&req); err != nil { //декодируем JSON из тела запроса в переменную req, при возникновении ошибки возвращаем код 400
+	//	http.Error(w, "Invalid request", http.StatusBadRequest)
+	//	return
+	//}
+	//task = req.Message
+	//w.WriteHeader(http.StatusOK) //устанавливаем статус ответа 200
+	//fmt.Fprintf(w, "Request: %s sent succesfully", task)
 }
 
 func main() {
+
+	InitDB()
+
+	DB.AutoMigrate(&Task{})
+
 	router := mux.NewRouter()
-	router.HandleFunc("/api/task", GetHandler).Methods("GET")
-	router.HandleFunc("/api/task", PostHandler).Methods("POST")
-
+	router.HandleFunc("/api/message", GetHandler).Methods("GET")
+	router.HandleFunc("/api/message", PostHandler).Methods("POST")
 	http.ListenAndServe(":8080", router)
-
 }
